d5c39afc535e0e6c3b22226062ab482c6ddabc7f
Fredrick10651 <Fredrick.mutsotso@student.moringaschool.com>
2021-06-08 23:09:29 +0300

    Adds multiplication,subtaction and divison features to our project

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-var add=function(number1,number2){
-    return number1+number2;
-    };
-    parseInt(prompt("Enter a number:"));
-    parseInt(prompt("Enter a number:"));
-    alert(add(number1,number2));
+var add = function(number1, number2) {
+    return number1 + number2;
+  };
+  
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  var result = add(number1, number2);
+  alert(result);
+
+  var subtract=function(number1,number2){
+      return number1-number2;
+  };
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  var result = subtract(number1, number2);
+  alert(result);
+
+  var multiplication=function(number1,number2){
+      return number1 * number2;
+  }
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  var result = multiplication(number1, number2);
+  alert(result);
+
+  var division=function(number1,number2){
+    return number1 * number2;
+}
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = division(number1, number2);
+alert(result);
+
+
+  
