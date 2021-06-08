2eebf204b6a0acf499f83d5343f3ba33bd49db8d
Fredrick10651 <Fredrick.mutsotso@student.moringaschool.com>
2021-06-08 22:43:24 +0300

    Adds addition and functions to our js file

---
+++ script.js
@@ -0,0 +1,6 @@
+var add=function(number1,number2){
+    return number1+number2;
+    };
+    parseInt(prompt("Enter a number:"));
+    parseInt(prompt("Enter a number:"));
+    alert(add(number1,number2));
